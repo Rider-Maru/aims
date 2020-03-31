0d57dcc533d9dbbd2cb0564a5ab2533bfd04d32b
Rider-Maru <hiroto.rider@outlook.jp>
2020-03-31 20:32:52 +0900

    ラム頁

---
+++ henshinRampage.js
@@ -0,0 +1,192 @@
+var ClickNum = 0;
+var AutorizeNum = 0;
+
+var isPushKey = false;
+var isAuthorizable = false;
+var onStandBy = false;
+var onAuthorize = false;
+
+var preRingNum;
+var preSlideNum;
+
+
+//var mySwiper.realIndex = 0;
+
+var threshold = 23;
+//videoタグを取得
+var video = document.getElementById("video");
+//取得するメディア情報を指定
+var medias = { audio: false, video: {} };
+
+var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+});
+
+function finishAudioLoading() {
+    if (navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/i)) {
+        medias.video.facingMode = { exact: "environment" };
+    } else {
+        medias.video.facingMode = { exact: "user" };
+    }
+
+    document.getElementById("str").textContent = "environment";
+
+    //getUserMediaを用いて、webカメラの映像を取得
+    navigator.mediaDevices.getUserMedia(medias).then(
+        function (stream) {
+            //videoタグのソースにwebカメラの映像を指定
+            video.srcObject = stream;
+
+        }
+    ).catch(
+        function (err) {
+            //カメラの許可がされなかった場合にエラー
+            window.alert("not allowed to use camera");
+        }
+    );
+}
+var canvas = document.getElementById("canvas");
+//ビデオのメタデータが読み込まれるまで待つ
+video.addEventListener("loadedmetadata", function (e) {
+    //canvasにカメラの映像のサイズを設定
+    canvas.width = video.videoWidth/3;
+    canvas.height = video.videoHeight/3;
+
+    //getContextで描画先を取得
+    var ctx = canvas.getContext("2d");
+    //毎フレームの実行処理
+    setInterval(function (e) {
+        if (mySwiper.realIndex != preSlideNum) {
+            playSERotate();
+        }
+        preSlideNum = mySwiper.realIndex;
+
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        var imagedata = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        var data = imagedata.data;
+        var allPicColor = 0;
+        for (var i = 0; i < canvas.height; i++) {
+            for (var j = 0; j < canvas.width; j++) {
+                var index = (i * canvas.width + j) * 4;
+                //元のピクセルカラーを取得
+                var r = data[index + 0];
+                var g = data[index + 1];
+                var b = data[index + 2];
+
+                //RGBをグレースケールに変換
+                var color = Math.round(r * 0.299 + g * 0.587 + b * 0.114);
+                data[index + 0] = color;
+                data[index + 1] = color;
+                data[index + 2] = color;
+                allPicColor += color;
+            }
+        }
+        var val = allPicColor / (canvas.height * canvas.width);
+        JudgeAutorize(val);
+        document.getElementById("debug").textContent = val;
+        ctx.putImageData(imagedata, 0, 0, 0, 0, canvas.width, canvas.height);
+    }, 33);
+        
+
+});
+
+
+function JudgeAutorize(value) {
+    if (value < threshold) {
+        if (!onAuthorize) {
+            onAuthorize = true;
+            document.getElementById("debug_bool").textContent = "true";
+        }
+ 
+    }
+    else {
+        if (onAuthorize) {
+            onAuthorize = false;
+            ringByCamera(1);
+            document.getElementById("debug_bool").textContent = "false";
+        }
+       
+    }
+    
+}
+
+// ========================================
+// 効果音を鳴らす（★今回のメインはこれ★）
+// ========================================
+function ring() {
+    if (preRingNum == mySwiper.realIndex) {
+        if (AutorizeNum == 2) {
+            SEstandbyStop();
+            playSECallFunction(mySwiper.realIndex);
+            setTimeout(function () {
+                if (onRingingStandby) isAuthorizable = true;
+            }, 1000)
+            AutorizeNum == 0;
+            AutorizeNum = 3;
+        } else if (AutorizeNum == 3) {
+            SEstandbyStop();
+            onStandBy = true;
+            playSEFinishReady(mySwiper.realIndex);
+            AutorizeNum = 4;
+        } else if (AutorizeNum == 4) {
+            SEstandbyStop();
+            playSECallFinish(mySwiper.realIndex);
+            AutorizeNum = 3;
+        }
+        else {
+            isAuthorizable = true;
+            SEstandbyStop();
+            AutorizeNum = 1;
+            playSECallKey(mySwiper.realIndex);
+            
+        }
+    }
+    else {
+        isAuthorizable = true;
+        SEstandbyStop();
+        AutorizeNum = 1;
+        playSECallKey(mySwiper.realIndex);
+        
+    }
+    preRingNum = mySwiper.realIndex;
+}
+
+
+function ringByCamera(callNum) {
+    var isRing = false;
+    if (!isAuthorizable) return;
+    if (onStandBy) SEstandbyStop();
+    if (callNum == 1 && AutorizeNum == 1) {
+        isRing = true;
+        onStandBy = true;
+        playSEBelt(mySwiper.realIndex);
+    }
+    if (isRing) {
+        isAuthorizable = false;
+        setTimeout(function () {
+            if (onRingingStandby || AutorizeNum == 3) isAuthorizable = true;
+        }, 1000)
+
+        if (AutorizeNum < 3) AutorizeNum++;
+    }
+}
+
+
+
+function SEstandbyStop() {
+    
+    //SE_authorize.pause();
+    //SE_authorize.currentTime = 0;
+    //SE_progrise.pause();
+    //SE_progrise.currentTime = 0;
+    onStandBy = false;
+    
+    stopSE();
+    if (AutorizeNum == 4) stopStandbyFinishSE();
+    else stopStandbySE();
+    
+}
+
+
+//-----------------------------------------------------------
+
---
+++ sounderRampage.js
@@ -0,0 +1,266 @@
+var soundArrayKey = [];
+var soundArrayCommon = [];
+var bufferListUpCommon = [];
+var bufferListUpKey = [];
+var nowplay;
+var nowplaynumKey;
+var nowplaynumCommon;
+var onRingingStandby = false;
+
+var AssaultWolfNum= 2;
+
+    function BufferLoader(context, urlList, callback) {
+        this.context = context;
+        this.urlList = urlList;
+        this.onload = callback;
+        this.bufferList = new Array();
+        this.loadCount = 0;
+    }
+    BufferLoader.prototype.loadBuffer = function (url, index) {
+        // Load buffer asynchronously
+        var request = new XMLHttpRequest();
+        request.open("GET", url, true);
+        request.responseType = "arraybuffer";
+        var loader = this;
+        request.onload = function () {
+            // Asynchronously decode the audio file data in request.response
+            loader.context.decodeAudioData(
+                request.response,
+                function (buffer) {
+                    if (!buffer) {
+                        alert('error decoding file data: ' + url);
+                        return;
+                    }
+                    loader.bufferList[index] = buffer;
+                    if (++loader.loadCount == loader.urlList.length)
+                        loader.onload(loader.bufferList);
+                },
+                function (error) {
+                    console.error('decodeAudioData error', error);
+                }
+            );
+        }
+        request.onerror = function () {
+            alert('BufferLoader: XHR error');
+        }
+        request.send();
+    }
+    BufferLoader.prototype.load = function () {
+    for (var i = 0; i < this.urlList.length; ++i)
+        this.loadBuffer(this.urlList[i], i);
+    }
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  
+//-------------------------------------------    
+'use strict';
+
+var context, analyser, frequencies, getByteFrequencyDataAverage,  draw;
+
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+context = new AudioContext();
+
+analyser = context.createAnalyser();
+frequencies = new Uint8Array(analyser.frequencyBinCount);
+
+getByteFrequencyDataAverage = function () {
+    analyser.getByteFrequencyData(frequencies);
+    return frequencies.reduce(function (previous, current) {
+        return previous + current;
+    }) / analyser.frequencyBinCount;
+};
+
+// 透明度を変更する要素
+var lightLayer = document.getElementsByClassName('square-button');
+// 可能な限り高いフレームレートで音量を取得し、透明度に反映する
+(draw = function () {
+    
+    // opacityの範囲である0〜1に変換
+    var val = (getByteFrequencyDataAverage() / 255) * (getByteFrequencyDataAverage() / 255) * 7;
+    for (var i = 0; i < lightLayer.length; i++){
+        lightLayer[i].style.opacity = val;
+    }
+    document.getElementById("debug_gain").textContent = val;
+    requestAnimationFrame(draw);
+})();
+//-------------------------------------------
+
+//context = new AudioContext();
+bufferLoader = new BufferLoader(
+    context,
+    [
+        'audio/authorize.mp3',
+        'audio/standbyLoop.mp3',
+        'audio/finishLoop.mp3',
+        'audio/finish.mp3',
+        'audio/blast.mp3',
+        'audio/rotate.mp3'
+    ],
+    finishedLoading
+);
+bufferLoader.load();
+function finishedLoading(bufferList) {
+    for (var i = 0; i < bufferList.length; i++) {
+        var source = context.createBufferSource();
+        source.buffer = bufferList[i];
+        bufferListUpCommon[i] = bufferList[i];
+        source.connect(context.destination);
+        soundArrayCommon.push(source);
+    }
+    bufferLoader = new BufferLoader(
+        context,
+        [
+            'audio/ballet.mp3',
+            'audio/shootingWolf.mp3',
+            'audio/shooting.mp3',
+            'audio/dash.mp3',
+            'audio/rushingCheetah.mp3',
+            'audio/rushing.mp3',
+            /*
+            'audio/.mp3',
+            'audio/.mp3',
+            'audio/.mp3',
+            */
+        ],
+        finishedLoading
+    );
+    bufferLoader.load();
+    function finishedLoading(bufferList) {
+        alert("ロードが完了しました");
+        finishAudioLoading();
+        for (var i = 0; i < bufferList.length; i++) {
+            var source = context.createBufferSource();
+            source.buffer = bufferList[i];
+            bufferListUpKey[i] = bufferList[i];
+            source.connect(context.destination);
+            soundArrayKey.push(source);
+        }
+    }
+}
+
+function playSECallKey(callNum) {
+    if (soundArrayKey[0] == null) {
+        alert('オーディオデータをロード中です');
+        return;
+    }
+    console.log("Key" + num);
+    var num = callNum * 3;
+
+    soundArrayKey[num].connect(analyser);
+    soundArrayKey[num].start(0);
+    soundArrayKey[num] = context.createBufferSource();
+    soundArrayKey[num].buffer = bufferListUpKey[num];
+    soundArrayKey[num].connect(context.destination);
+}
+function playSERotate() {
+    if (soundArrayKey[0] == null) {
+        alert('オーディオデータをロード中です');
+        return;
+    }
+    console.log("Key" + num);
+    var num = 5;
+
+    soundArrayCommon[num].connect(analyser);
+    soundArrayCommon[num].start(0);
+    soundArrayCommon[num] = context.createBufferSource();
+    soundArrayCommon[num].buffer = bufferListUpKey[num];
+    soundArrayCommon[num].connect(context.destination);
+}
+
+function playSECallFunction(callNum) {
+    var num = 1 + callNum * 3;
+    nowplaynumKey = num;
+    console.log("Function" + num);
+    soundArrayKey[nowplaynumKey].connect(analyser);
+    soundArrayKey[nowplaynumKey].start(0);
+}
+
+function playSECallFinish(callNum) {
+    var num = 2 + callNum * 3;
+    stopSE();
+    nowplaynumCommon = 3;
+    if (callNum == AssaultWolfNum) nowplaynumCommon = 6;
+    console.log("Finish" + num);
+    soundArrayCommon[nowplaynumCommon].connect(analyser);
+    soundArrayCommon[nowplaynumCommon].start(0);
+    soundArrayCommon[nowplaynumCommon].onended = function () {
+        if (nowplaynumCommon == null) return;
+        stopSE();
+        nowplaynumCommon = null;
+        nowplaynumKey = num;
+        soundArrayKey[num].connect(analyser);
+        soundArrayKey[num].start(0);
+        soundArrayKey[num].onended = function () {
+            if (nowplaynumKey == null) return;
+            stopSE();
+            nowplaynumCommon = 4;
+            nowplaynumKey = null;
+            soundArrayCommon[nowplaynumCommon].connect(analyser);
+            soundArrayCommon[nowplaynumCommon].start(0);
+        }
+    }
+}
+
+function playSEBelt(callNum) {
+    var num = 0;
+    if (callNum == AssaultWolfNum) num = 5;
+
+    nowplaynumCommon = num;
+    console.log("Belt" + num);
+    soundArrayCommon[num].connect(analyser);
+    soundArrayCommon[num].start(0);
+    soundArrayCommon[num].onended = function () {
+        if (nowplaynumCommon == null) return;
+        soundArrayCommon[1].loop = true;
+        soundArrayCommon[1].start(0);
+        onRingingStandby = true;
+    }
+}
+function playSEFinishReady(callNum) {
+    var num = callNum * 3;
+
+    nowplaynumKey = num;
+    console.log("Belt" + num);
+    soundArrayKey[num].connect(analyser);
+    soundArrayKey[num].start(0);
+    soundArrayKey[num].onended = function () {
+        if (nowplaynumKey == null) return;
+        soundArrayCommon[2].loop = true;
+        soundArrayCommon[2].start(0);
+        onRingingStandby = true;
+    }
+}
+
+function stopSE() {
+    if (nowplaynumCommon != null) {
+        soundArrayCommon[nowplaynumCommon].stop();
+        soundArrayCommon[nowplaynumCommon] = context.createBufferSource();
+        soundArrayCommon[nowplaynumCommon].buffer = bufferListUpCommon[nowplaynumCommon];
+        soundArrayCommon[nowplaynumCommon].connect(context.destination);
+        nowplaynumCommon = null
+    }
+    if (nowplaynumKey != null) {
+        soundArrayKey[nowplaynumKey].stop();
+        soundArrayKey[nowplaynumKey] = context.createBufferSource();
+        soundArrayKey[nowplaynumKey].buffer = bufferListUpKey[nowplaynumKey];
+        soundArrayKey[nowplaynumKey].connect(context.destination);
+        nowplaynumKey = null;
+    }
+}
+
+function stopStandbySE() {
+    if (!onRingingStandby) return;
+    soundArrayCommon[1].stop();
+    soundArrayCommon[1] = context.createBufferSource();
+    soundArrayCommon[1].buffer = bufferListUpCommon[1];
+    soundArrayCommon[1].connect(context.destination);
+
+    onRingingStandby = false;
+}
+function stopStandbyFinishSE() {
+    if (!onRingingStandby) return;
+    soundArrayCommon[2].stop();
+    soundArrayCommon[2] = context.createBufferSource();
+    soundArrayCommon[2].buffer = bufferListUpCommon[2];
+    soundArrayCommon[2].connect(context.destination);
+    onRingingStandby = false;
+}
